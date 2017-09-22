a3688ea12b82a6ee5809708be9dc67ae6f928d5d
Elo <wanang.elodie@hotmail.fr>
2017-09-22 17:05:36 +0200

    Music added

--- app.js
+++ app.js
@@ -12,7 +12,14 @@ function updateQuestion() {
   $("section#game > .titlequestion > h2").html("Question" + " " + n);
 
       // game.currentQuestion().answers
-  optionClicked();
+
+  if (game.turnsPlayed %2 === 0) {
+    $(".score2").removeClass("turn");
+    $(".score1").addClass("turn");
+  } else {
+    $(".score1").removeClass("turn");
+    $(".score2").addClass("turn");
+  }
 
 
 }
@@ -25,26 +32,57 @@ function updateAnswers () {
   return answersHtml;
 }
 
-function optionClicked () {
-  $(".choice").click(function(event) {
-    $(this).css("background-color", "#913720");
-    var selectedChoice = parseInt($(this).attr('choice'));
+function registerClickChoiceHandler() {
+  var ongoingTurn = 0;
+  $(".answers").click(function(event) {
+    var choiceDiv = $(event.target);
+    if(!choiceDiv.hasClass('choice')) {
+      return;
+    }
+    if (ongoingTurn !== game.turnsPlayed) {
+      return;
+    }
+    choiceDiv.css("background-color", "#913720");
+    var selectedChoice = parseInt(choiceDiv.attr('choice'));
     var isCorrectAnswer = game.play(selectedChoice);
     updateScore();
     if (isCorrectAnswer) {
-      $('.question h4').text("Bonne réponse!");
+      $('.question h4').text("You killed it !");
+      $('.question').addClass("goodanswer");
       // TODO: change the background in green
     }
     else {
-      $('.question h4').text("Mauvaise réponse...");
+      $('.question h4').text("What's wrong with you ?!");
       // TODO: change the background in red
+      $('.question').addClass("wronganswerT");
     }
     setTimeout(function() {
       if (game.isGoing) {
         updateQuestion();
+        ongoingTurn++;
+        $('.question').removeClass("goodanswer");
+        $('.question').removeClass("wronganswerT");
+        $("score2").addClass("selected");
       }
       else {
-        console.log("OVER");
+        updateScore();
+        if (game.winner()) {
+          $('.question, .answers, .titlequestion').hide();
+          $('#winner').show();
+          if (game.winner().length == 2) {
+            $('.winnername').text("Your are both winners ! ");
+
+          }
+          else {
+            $('.winnername').text( "Congrats " + game.winner()[0].name + " !" + " You've just became my chocolate mate :) !");
+            $('.winnername').addClass(".winnername");
+            var audio = $("#my_audio").get(0);
+            audio.currentTime=0;
+            audio.play();
+          }
+        }
+
+          console.log("OVER");
         // TODO: change the page to show the winner
       }
     }, 2000);
@@ -58,15 +96,24 @@ function updateScore() {
 }
 
 function startGame() {
+  $("#my_audio2").get(0).play();
+  registerClickChoiceHandler();
   $("#welcome").hide();
+  $("#winner").hide();
   $("#game").show();
   updateQuestion();
+  $("#my_audio").get(0).pause();
+
+
+
 }
 
 $(document).ready(function() {
+  $("#my_audio").get(0).play();
   game = new ChocoGame();
-
+$("#welcome > .regle").hide().delay(2000).fadeIn(2000);
   $("#game").hide();
+    $("#winner").hide();
   // TODO récupérer le boutton arrow et
   // le faire changer de section
   $("#downArrow").click(function() {
--- chocoGame.js
+++ chocoGame.js
@@ -2,22 +2,115 @@
 var quiz = [
   {
     question: 'Quels sont les bienfaits du chocolat ?',
-
-    answers: ['antioxydant','anti-fatigue','exitant'],
-
+    answers: ['anti-oxydant','anti-fatigue','exitant'],
     correctAnswer: 0,
   },
 
   {
-    question: "Le chocolat est-il l'allier des femmes enceintes ?",
+    question: "Le chocolat est-il l'allié des femmes enceintes ?",
     answers: ['oui','non'],
     correctAnswer: 0,
   },
+  {
+    question: "Quel est le minéral principal? ",
+    answers: ['calcium','sodium', 'magnésium'],
+    correctAnswer: 2,
+  },
+  {
+    question: "Le chocolat a t-il toujours existé en France ?",
+    answers: ['oui','non'],
+    correctAnswer: 1,
+  },
+  {
+    question: "la capacité anti-oxydante du cacao est plus faible que celle...",
+    answers: ['thé noir','vin','aucune'],
+    correctAnswer: 2,
+  },
+  {
+    question: "Combien de type de chocolat existe t-il ?",
+    answers: ['2','3','4'],
+    correctAnswer: 2,
+  },
+  {
+    question: "Le chocolat est bon pour ... ?",
+    answers: ['lutter contre Alzheimer','empêcher les bêtises','être meilleur en development web'],
+    correctAnswer: 0,
+  },
+  {
+    question: "Mais où a donc été découvert le chocolat ?",
+    answers: ['Amérique du Sud','Afrique subsaharienne', "Suisse"],
+    correctAnswer: 0,
+  },
+  {
+    question: "Quel composant rend le chocolat suisse si unique ?",
+    answers: ['son lait','son amertume', 'sa poudre de cacao'],
+    correctAnswer: 0,
+  },
+  {
+    question: "La production excessive de chocolat contribue à : ",
+    answers: ['la déforestation','la malnutrition', 'la fin du monde'],
+    correctAnswer: 0,
+  },
+  {
+    question: "Quel label contribue à une production plus responsable du chocolat ?",
+    answers: ['UTZ','fuck les Agro!', 'For a better world'],
+    correctAnswer: 0,
+  },
+
+  {
+    question: "Quel est le nom original du chocolat ?",
+    answers: ['Chocolate','Obsesion', "Xocoatl"],
+    correctAnswer: 2,
+  },
+
+
+  {
+    question: "Un-e ami-e est sur le point d'aller courir: lui conseillerez-vous un carré de chocolat ?",
+    answers: ['non ca va l\'endormir ...','Oui ca va l\'aider à recupérer :) ', "Bah pourquoi pas lui offrir la tablette ?!"],
+    correctAnswer: 1,
+  },
+
+  {
+    question: "Le chocolat se marie très bien avec: ",
+    answers: ['le poivre','la crevette', "les deux"],
+    correctAnswer: 2,
+  },
+
+  { question: "Quel est le chocolat preféré des patissiers ?",
+    answers: ['le chocolat au lait','le chocolat noir','le chocolat de couverture'],
+    correctAnswer: 2,
+  },
+
+    { question: "Quelle est la marque de chocolat preférée des Francais ?",
+      answers: ['Milka','Côte d\'or','Ferrero'],
+      correctAnswer: 1,
+    },
+    { question: "Le chocolat blanc est -il du vrai chocolat ?",
+      answers: ['non','oui'],
+      correctAnswer: 0,
+  },
+      {
+        question: "Quel est le duo qui nous a fait dansé tout l'été sur le chocolat ? ",
+        answers: ['Aya Nakamura et Niska','l\'artiste - Awa Imani', 'Carla et Johnny '],
+        correctAnswer: 1,
+      },
+
+
+  { question: "Quel est mon chocolat préféré?",
+    answers: ['72%','50%', "99%"],
+    correctAnswer: 0,
+  },
+
+  { question: "Quel est mon association préféré ?",
+    answers: ['chocolat-citron','chocolat-piment', "chocolat-pamplemousse"],
+    correctAnswer: 2,
+  },
+
 ];
 
 function ChocoGame() {
   this.quiz = quiz;
-  this.players = [{ name: 'Joueur1', score: 0 }, { name: 'Joueur2', score: 0 }];
+  this.players = [{ name: 'Joueur 1', score: 0 }, { name: 'Joueur 2', score: 0 }];
   this.isGoing = true;
   this.currentPlayer = 0;
   this.turnsPlayed = 0;
@@ -46,8 +139,6 @@ ChocoGame.prototype.play = function(answerSelected) {
     }
   return good;
 };
-
-
 
 
 ChocoGame.prototype.winner = function() {
